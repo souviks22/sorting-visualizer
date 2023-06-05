c725225e754177efc391275d9c3f4d0ef9389556
Souvik Sarkar <souviksarkar2k3@gmail.com>
2023-06-05 15:25:54 +0530

    Tiles are made responsive.

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import SpinnerBox from '@/components/SpinnerBox'
 
 const Home = ({ algos }) => {
   return (<div className='flex flex-col'>
-    <h1 className='display-1 text-center p-5 m-3'>Sorting Visualization</h1>
-    <div className='flex justify-around p-20'>
+    <h1 className='display-1 text-center p-5 m-3 z-20'>Sorting Visualization</h1>
+    <div className='tiles flex justify-around p-20'>
       {algos.map(algo => <Tile key={algo.name} {...algo} />)}
     </div>
     <SpinnerBox />
